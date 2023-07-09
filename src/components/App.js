84aeceabad8f512c47d67e8f6857716d84875521
anitha-anii <anithachowdary355@gmail.com>
2023-07-09 15:14:11 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+// import { update } from "cypress/types/lodash";
 
 const App = () => {
+  const [text,setText] =useState('');
+  const[list,setList] =useState([]);
+
+  function update(e){
+    setText(e.target.value);
+  }
+  function display(){
+    if(text.trim()!==''){
+      setList([...list,text]);
+      setText('');
+    }
+  }
+
+  function removed(index){
+    let upadelist =[...list];
+    upadelist.splice(index,1);
+    setList(upadelist);
+  }
+
   return (
-    <div>
+    <div className="to-do">
         {/* Do not remove the main div */}
+       <h2>To-Do List</h2>
+       <input type="text" value={text} onChange={update} />
+       <button onClick={display}>Add Todo</button>
+      
+      {list.map((item,index)=>(
+          <div key={index} id="div">
+          <p>{item}</p>
+          <button onClick={()=>removed(index)}>Delete</button>
+          </div>
+
+    ))
+      }
+
     </div>
   )
 }
