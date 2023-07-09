b1f532fb212e70336f610791f3bd2b73fc9c3d9f
anitha-anii <anithachowdary355@gmail.com>
2023-07-09 15:56:33 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,44 @@
-
-import React,{useState} from "react";
+import React, { useState } from 'react';
 import './../styles/App.css';
-// import { update } from "cypress/types/lodash";
+
 
 const App = () => {
-  const [text,setText] =useState('');
-  const[list,setList] =useState([]);
+  const [text, setText] = useState('');
+  const [list, setList] = useState([]);
 
-  function update(e){
+  const handleInputChange = (e) => {
     setText(e.target.value);
-  }
-  function display(){
-    if(text.trim()!==''){
-      setList([...list,text]);
+  };
+
+  const handleAddTodo = () => {
+    if (text.trim() !== '') {
+      setList([...list, text]);
       setText('');
     }
-  }
+  };
 
-  function removed(index){
-    let upadelist =[...list];
-    upadelist.splice(index,1);
-    setList(upadelist);
-  }
+  const handleRemoveTodo = (index) => {
+    const updatedList = [...list];
+    updatedList.splice(index, 1);
+    setList(updatedList);
+  };
 
   return (
     <div className="to-do">
-        {/* Do not remove the main div */}
-       <h2>To-Do List</h2>
-       <input type="text" value={text} onChange={update} />
-       <button onClick={display}>Add Todo</button>
-      
-      {list.map((item,index)=>(
-          <ul key={index} id="div">
-          <li>{item}</li>
-          <button onClick={()=>removed(index)}>Delete</button>
-          </ul>
+      <h2>To-Do List</h2>
+      <input type="text" value={text} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Add Todo</button>
 
-    ))
-      }
-
+      <ul>
+        {list.map((item, index) => (
+          <li key={index} id='div'>
+            {item}
+            <button onClick={() => handleRemoveTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
